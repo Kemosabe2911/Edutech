adbdb01dbab694fdbfebf9c13675088d231329df
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-19 13:17:41 +0530

    Update toggle btn

---
+++ js/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#icon').click(function(){
+        $('ul').toggleClass('show');
+    });
+});
+
+/*Toggle Close*/
+$(document).ready(function(){
+    $('#icon').click(function(){
+        $('#icon').toggleClass('close');
+    });
+});
