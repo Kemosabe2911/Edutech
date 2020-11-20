bda1ff7855e1ae360f7002d42f71e6c58677fb70
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-20 13:18:51 +0530

    slide

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ anime({
     loop: true,
     direction: 'alternate'
 })
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
